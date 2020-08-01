5a5a7a3adb08f6dd356bc82874aacb33e93f09aa
brianndjojo <38829497+brianndjojo@users.noreply.github.com>
2020-08-01 11:59:24 +0800

    01/08/20

--- liveTranslation/resources/js/config.js
+++ liveTranslation/resources/js/config.js
@@ -2,7 +2,9 @@
 var config = {
     currentCarousel:0,
     prevCarousel:0,
+    //publication inputs
     publications: [],
+    //publication placeholder on cv
     publicationPlaceholders:[],
     others: [],
     otherPlaceholders:[],
